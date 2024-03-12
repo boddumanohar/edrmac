0c810ac82b7e4d6d94f631b2668deaf165751496
Manohar Reddy <b.manu199@gmail.com>
2024-03-12 14:31:06 +0100

    push to es. todo: push in the form of log streams

--- go.mod
+++ go.mod
@@ -2,13 +2,7 @@ module edrmac
 
 go 1.22.0
 
-require github.com/elastic/go-elasticsearch/v8 v8.12.1
-
 require (
-	github.com/elastic/elastic-transport-go/v8 v8.4.0 // indirect
-	github.com/go-logr/logr v1.3.0 // indirect
-	github.com/go-logr/stdr v1.2.2 // indirect
-	go.opentelemetry.io/otel v1.21.0 // indirect
-	go.opentelemetry.io/otel/metric v1.21.0 // indirect
-	go.opentelemetry.io/otel/trace v1.21.0 // indirect
+	github.com/elastic/go-elasticsearch v0.0.0
+	github.com/elastic/go-elasticsearch/v7 v7.17.10
 )
--- main.go
+++ main.go
@@ -1,83 +1,89 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
+	"bufio"
+	"bytes"
 	"fmt"
 	"os/exec"
 	"strings"
-	"time"
 
-	"github.com/elastic/go-elasticsearch/v8"
-	"github.com/elastic/go-elasticsearch/v8/esapi"
+	elasticsearch7 "github.com/elastic/go-elasticsearch/v7"
 )
 
 func main() {
 	// Initialize Elasticsearch client
-	esCfg := elasticsearch.Config{
+	esCfg := elasticsearch7.Config{
 		Addresses: []string{"http://localhost:9200"},
 	}
-	esClient, err := elasticsearch.NewClient(esCfg)
+	esClient, err := elasticsearch7.NewClient(esCfg)
 	if err != nil {
 		fmt.Println("Error creating Elasticsearch client:", err)
 		return
 	}
 
+	fmt.Println("COnnected to ES")
 	// Command to execute
-	cmd := exec.Command("log", "stream", "--style", "json")
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		fmt.Println("Error creating stdout pipe:", err)
-		return
-	}
+	// cmd := exec.Command("log", "stream", "--style", "json").Output()
+	// stdout, err := cmd.StdoutPipe()
+	// if err != nil {
+	// 	fmt.Println("Error creating stdout pipe:", err)
+	// 	return
+	// }
 
-	// Start the command
-	err = cmd.Start()
-	if err != nil {
-		fmt.Println("Error starting command:", err)
-		return
-	}
+	// // Start the command
+	// err = cmd.Start()
+	// if err != nil {
+	// 	fmt.Println("Error starting command:", err)
+	// 	return
+	// }
 
-	// Read and process output
-	decoder := json.NewDecoder(stdout)
+	// // Read and process output
+	// decoder := json.NewDecoder(stdout)
 	for {
-		var event map[string]interface{}
-		if err := decoder.Decode(&event); err != nil {
-			fmt.Println("Error decoding JSON:", err)
-			break
-		}
+		fmt.Println("Started decoder")
+		// var event map[string]interface{}
+		// if err := decoder.Decode(&event); err != nil {
+		// 	fmt.Println("Error decoding JSON:", err)
+		// 	break
+		// }
 
-		fmt.Println("Received event:", event)
+		// cmd, err := exec.Command("log", "stream", "--style", "json").Output()
+		// if err != nil {
+		// 	panic(err)
+		// }
+
+		args := "stream --style json"
+		cmd := exec.Command("log", strings.Split(args, " ")...)
+
+		stdout, _ := cmd.StdoutPipe()
+		cmd.Start()
+
+		scanner := bufio.NewScanner(stdout)
+		scanner.Split(bufio.ScanWords)
+		for scanner.Scan() {
+			m := scanner.Text()
+			fmt.Println("Received event:", m)
+			index := "eclexys_edr_mac_index"
+			// data, _ := json.Marshal(event)
+			esClient.Index(index, bytes.NewReader([]byte(m)))
+		}
+		cmd.Wait()
+
+		// Start the command
+		// err = cmd.Start()
+		// if err != nil {
+		// 	fmt.Println("Error starting command:", err)
+		// 	return
+		// }
 
 		// Send event to Elasticsearch
-		index := "eclexys_edr_mac_index"
-		req := esapi.IndexRequest{
-			Index:      index,
-			DocumentID: "",
-			Body:       strings.NewReader(fmt.Sprintf("%v", event)),
-			Refresh:    "true",
-		}
 
-		// Perform the request
-		res, err := req.Do(context.Background(), esClient)
-		if err != nil {
-			fmt.Println("Error indexing document:", err)
-		} else {
-			defer res.Body.Close()
-			if res.IsError() {
-				fmt.Printf("Error indexing document: %s", res.Status())
-			} else {
-				fmt.Println("Document indexed successfully.")
-			}
-		}
-
-		time.Sleep(1 * time.Second)
 	}
 
-	err = cmd.Wait()
-	if err != nil {
-		fmt.Println("Error waiting for command to finish:", err)
-	}
+	// err = cmd.Wait()
+	// if err != nil {
+	// 	fmt.Println("Error waiting for command to finish:", err)
+	// }
 }
 
 // TODO: Forward the last 1 minute logs
