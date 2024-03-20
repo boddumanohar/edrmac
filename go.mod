b1fe159e64c3f43d5dd12db0ca471e21a4519fb2
Manohar Boddu <manohar.boddu@gymshark.com>
2024-03-20 22:07:28 +0100

    push to ElasticSearch

--- go.mod
+++ go.mod
@@ -3,6 +3,15 @@ module edrmac
 go 1.22.0
 
 require (
-	github.com/elastic/go-elasticsearch v0.0.0
-	github.com/elastic/go-elasticsearch/v7 v7.17.10
+	github.com/MaxSchaefer/macos-log-stream v0.0.0-20221124183856-194e68fd7a89
+	github.com/elastic/go-elasticsearch/v8 v8.12.1
+)
+
+require (
+	github.com/elastic/elastic-transport-go/v8 v8.4.0 // indirect
+	github.com/go-logr/logr v1.3.0 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	go.opentelemetry.io/otel v1.21.0 // indirect
+	go.opentelemetry.io/otel/metric v1.21.0 // indirect
+	go.opentelemetry.io/otel/trace v1.21.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,89 +1,49 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
+	"encoding/json"
 	"fmt"
-	"os/exec"
-	"strings"
 
-	elasticsearch7 "github.com/elastic/go-elasticsearch/v7"
+	"github.com/MaxSchaefer/macos-log-stream/pkg/mls"
+	elasticsearch8 "github.com/elastic/go-elasticsearch/v8"
+)
+
+const (
+	INDEX = "logs-eclexys_edr_mac_index"
 )
 
 func main() {
 	// Initialize Elasticsearch client
-	esCfg := elasticsearch7.Config{
+	esCfg := elasticsearch8.Config{
 		Addresses: []string{"http://localhost:9200"},
 	}
-	esClient, err := elasticsearch7.NewClient(esCfg)
+	esClient, err := elasticsearch8.NewClient(esCfg)
 	if err != nil {
 		fmt.Println("Error creating Elasticsearch client:", err)
 		return
 	}
 
-	fmt.Println("COnnected to ES")
-	// Command to execute
-	// cmd := exec.Command("log", "stream", "--style", "json").Output()
-	// stdout, err := cmd.StdoutPipe()
-	// if err != nil {
-	// 	fmt.Println("Error creating stdout pipe:", err)
-	// 	return
-	// }
+	fmt.Println("Started decoder")
 
-	// // Start the command
-	// err = cmd.Start()
-	// if err != nil {
-	// 	fmt.Println("Error starting command:", err)
-	// 	return
-	// }
+	logs := mls.NewLogs()
+	// logs.Predicate = ""
 
-	// // Read and process output
-	// decoder := json.NewDecoder(stdout)
-	for {
-		fmt.Println("Started decoder")
-		// var event map[string]interface{}
-		// if err := decoder.Decode(&event); err != nil {
-		// 	fmt.Println("Error decoding JSON:", err)
-		// 	break
-		// }
-
-		// cmd, err := exec.Command("log", "stream", "--style", "json").Output()
-		// if err != nil {
-		// 	panic(err)
-		// }
-
-		args := "stream --style json"
-		cmd := exec.Command("log", strings.Split(args, " ")...)
-
-		stdout, _ := cmd.StdoutPipe()
-		cmd.Start()
-
-		scanner := bufio.NewScanner(stdout)
-		scanner.Split(bufio.ScanWords)
-		for scanner.Scan() {
-			m := scanner.Text()
-			fmt.Println("Received event:", m)
-			index := "eclexys_edr_mac_index"
-			// data, _ := json.Marshal(event)
-			esClient.Index(index, bytes.NewReader([]byte(m)))
-		}
-		cmd.Wait()
-
-		// Start the command
-		// err = cmd.Start()
-		// if err != nil {
-		// 	fmt.Println("Error starting command:", err)
-		// 	return
-		// }
-
-		// Send event to Elasticsearch
-
+	if err := logs.StartGathering(); err != nil {
+		panic(err)
 	}
 
-	// err = cmd.Wait()
-	// if err != nil {
-	// 	fmt.Println("Error waiting for command to finish:", err)
-	// }
-}
+	for log := range logs.Channel {
+		fmt.Println(log)
+		data, err := json.Marshal(log)
+		if err != nil {
+			panic(err)
+		}
 
-// TODO: Forward the last 1 minute logs
+		res, err := esClient.Index(INDEX, bytes.NewReader(data))
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Indexed document with ID: %s\n", res.String())
+	}
+}
